f2fd71a0776403b155f22efd9f6ad1667ba6ccb9
Sukh-123 <115053139+Sukh-123@users.noreply.github.com>
2024-10-02 14:39:44 +1300

    filtering accept/reject

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ModerationModule } from './moderation/moderation.module';
+import { ConfigModule } from '@nestjs/config';
+import { ArticleController } from './articles/article.controller';
+import { ArticleService } from './articles/article.service';
+import { Article, ArticleSchema } from './articles/schemas/article.schema';
+import { AcceptedArticle, AcceptedArticleSchema } from './articles/schemas/accepted-article.schema'; 
+import { RejectedArticle, RejectedArticleSchema } from './articles/schemas/rejected-article.schema'; 
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/speed-db'), // Your MongoDB connection
-    ModerationModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI),
+    MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }]),
+    MongooseModule.forFeature([{ name: AcceptedArticle.name, schema: AcceptedArticleSchema }]),
+    MongooseModule.forFeature([{ name: RejectedArticle.name, schema: RejectedArticleSchema }]),
   ],
 })
 export class AppModule {}
