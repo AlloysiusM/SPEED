417170c6de20f3fa8429f82d27b35f6e1da73746
AlloysiusM <albrentmanlutac@gmail.com>
2024-10-17 16:36:15 +1300

    testing contoller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-//import { AppController } from './app.controller';
+import { AppController } from './app.controller';
 //import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
